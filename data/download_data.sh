44791289e4c882b1cf365403fd2954551e5b2349
Michele Doro <michele.doro@unipd.it>
2022-08-25 12:25:23 +0200

    created data folder and included script inside

---
+++ data/download_data.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+DATA_URL=${DATA_URL:-https://userswww.pd.infn.it/~mdoro/arqus/data}
+
+wget \
+	-R "index.html*,robots*"\
+	--no-host-directories --cut-dirs=1 \
+	--no-parent \
+	--no-verbose \
+	--recursive \
+	--directory-prefix=. \
+	"$DATA_URL"
+
+mv arqus/data .
+rm -rf arqus
