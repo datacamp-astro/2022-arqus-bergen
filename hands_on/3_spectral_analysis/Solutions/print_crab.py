a0c8fe028ba3f8d9ac3b35a379e0f8f5d15918a6
Ivana Batkovic <ivana.batkovic@phd.unipd.it>
2022-08-25 14:22:27 +0200

    Adding the notebook for Spectram analysis of Crab Nebula

---
+++ hands_on/3_spectral_analysis/Solutions/print_crab.py
@@ -0,0 +1,1 @@
+print(observations_crab)
---
+++ hands_on/3_spectral_analysis/Solutions/pwl_model_points_plot.py
@@ -0,0 +1,4 @@
+flux_points_crab_dataset_pwl = FluxPointsDataset(
+    data=flux_points_crab_pwl, models=datasets.models
+)
+flux_points_crab_dataset_pwl.plot_fit()
