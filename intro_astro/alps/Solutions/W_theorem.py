4fa2221150c8f8325130fb334ba0c3b3c2eafa31
Ivana Batković <77786626+ibatkovi@users.noreply.github.com>
2022-09-04 11:44:24 +0200

    Update W_theorem.py

--- intro_astro/alps/Solutions/W_theorem.py
+++ intro_astro/alps/Solutions/W_theorem.py
@@ -1,2 +1,4 @@
-Write the Wilks' theorem here
+As the sample size approaches infinity, the distribution of the test statistic −2logΛ-2 log(Lambda) asymptotically approaches the chi-squared χ_2\chi^2 distribution under the null hypothesis.
+Lambda  denotes the likelihood ratio, and the χ_\chi^2 distribution has degrees of freedom equal to the difference in dimensionality of the full parameter space and the subset of the parameter space associated with null hypothesis. 
+This result means that one can compute the likelihood ratio Lambda  for the data and compare −2log(Λ)-2\log(\Lambda) to the χ2\chi ^{2} value corresponding to a desired statistical significance as an approximate statistical test.
 
